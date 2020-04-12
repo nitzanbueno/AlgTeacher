1d05eaaab139bff7078d6ead0417ea381405a1db
Nitzan Bueno <nitzan.bueno@gmail.com>
2020-04-12 14:28:12 +0300

    Add navigation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,28 @@
+import "react-native-gesture-handler";
 import React from "react";
 import { MainScreen } from "./MainScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+function AppNavigationStack() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Main"
+                    component={MainScreen}
+                    options={{ title: "AlgTeacher" }}
+                />
+                {/* <Stack.Screen name="Add" component={AddScreen} /> */}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
 
 export default function App() {
-  return <MainScreen />;
+    return (
+        <AppNavigationStack />
+    );
 }
