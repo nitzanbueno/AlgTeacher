dafea8cddfab0b1b213663886394f35ff23ab0ee
Nitzan Bueno <nitzan.bueno@gmail.com>
2020-04-16 23:51:38 +0300

    Remove the scramble generator and cubejs

--- App.tsx
+++ App.tsx
@@ -4,19 +4,11 @@ import { MainScreen } from "./MainScreen";
 import { TestScreen } from "./TestScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Cube from "cubejs";
 import { AddScreen } from "./AddScreen";
 
 const Stack = createStackNavigator();
 
 class AppNavigationStack extends Component {
-    componentDidMount() {
-        // This is EXTREMELY slow - ~30s.
-        // I have to make it work in the background because it freezes everything and you think the app didn't even open.
-        // It's TERRIBLE.
-        Cube.initSolver();
-    }
-
     render() {
         return (
             <NavigationContainer>
