c13db35ed9595b2bb294dac1e23bc3d04e5038b1
Nitzan Bueno <nitzan.bueno@gmail.com>
2020-07-16 22:25:17 +0300

    Refactor TimeAttackEnd and TimeAttackStorage
    Refactor time attack end screen with hooks
    Refactor TimeAttackStorage to be a static class

--- src/TimeAttackStorage.ts
+++ src/TimeAttackStorage.ts
@@ -8,35 +8,37 @@ function hashCases(cases: Case[]) {
     return HashCode(JSON.stringify(cases.sort((a, b) => a.id - b.id).map(c => c.algorithm)));
 }
 
-type HighScoreType = {totalTime: number; solveCount: number};
+export type HighScoreType = {totalTime: number; solveCount: number};
 
-export async function getHighScore(caseList: Case[]): Promise<HighScoreType | null> {
-    const key = HIGHSCORE_KEY + hashCases(caseList);
-    const result = await AsyncStorage.getItem(key);
-    console.log(key, result);
-
-    if (result == null) return null;
-
-    let parsedResult: any = {};
-
-    try {
-        parsedResult = JSON.parse(result);
-    } catch (SyntaxError) {
-        await AsyncStorage.removeItem(key);
-        return null;
+export default class TimeAttackStorage {
+    static async FetchHighScore(caseList: Case[]): Promise<HighScoreType | null> {
+        const key = HIGHSCORE_KEY + hashCases(caseList);
+        const result = await AsyncStorage.getItem(key);
+        console.log(key, result);
+    
+        if (result == null) return null;
+    
+        let parsedResult: any = {};
+    
+        try {
+            parsedResult = JSON.parse(result);
+        } catch (SyntaxError) {
+            await AsyncStorage.removeItem(key);
+            return null;
+        }
+    
+        if (!parsedResult.totalTime || !parsedResult.solveCount) {
+            await AsyncStorage.removeItem(key);
+            return null;
+        }
+    
+        return parsedResult;
     }
 
-    if (!parsedResult.totalTime || !parsedResult.solveCount) {
-        await AsyncStorage.removeItem(key);
-        return null;
+    static async SaveHighScore(caseList: Case[], highScore: HighScoreType) {
+        const key = HIGHSCORE_KEY + hashCases(caseList);
+        const value = JSON.stringify(highScore);
+        console.log('SET', key, value);
+        await AsyncStorage.setItem(key, value);
     }
-
-    return parsedResult;
-}
-
-export async function setHighScore(caseList: Case[], highScore: HighScoreType) {
-    const key = HIGHSCORE_KEY + hashCases(caseList);
-    const value = JSON.stringify(highScore);
-    console.log('SET', key, value);
-    await AsyncStorage.setItem(key, value);
 }
