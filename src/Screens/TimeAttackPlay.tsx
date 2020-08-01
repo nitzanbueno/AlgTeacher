a16b946f9da7be89b3008c40da157f718c57945c
Nitzan Bueno <nitzan.bueno@gmail.com>
2020-08-01 19:25:00 +0300

    Restyle time attack screen

--- src/Screens/TimeAttackPlay.tsx
+++ src/Screens/TimeAttackPlay.tsx
@@ -1,72 +1,78 @@
-import React, { useState, FC, useEffect, useContext } from 'react';
-import {Component} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import {TouchableNativeFeedback} from 'react-native-gesture-handler';
-import {TOUCHABLE_BACKGROUND, Case} from '../Models';
-import { CaseStoreContext } from '../CaseStore';
-import {GenerateScramble, MirrorAlgorithm} from '../ScrambleLib';
-import {GetTimeText, ShuffleArray, RandomChoice} from '../Utils';
-import Timer from '../CommonComponents/Timer';
-import { observer } from 'mobx-react';
+import React, {useState, FC, useEffect, useContext} from "react";
+import {Text, View, StyleSheet, TouchableWithoutFeedback, TouchableNativeFeedback} from "react-native";
+import {TOUCHABLE_BACKGROUND, Case} from "../Models";
+import {CaseStoreContext} from "../CaseStore";
+import {GenerateScramble, MirrorAlgorithm} from "../ScrambleLib";
+import {GetTimeText, ShuffleArray, RandomChoice} from "../Utils";
+import Timer from "../CommonComponents/Timer";
+import {observer} from "mobx-react";
 
-const UNDEFINED_SCRAMBLE_TEXT: string = 'Loading...';
+const UNDEFINED_SCRAMBLE_TEXT: string = "Loading...";
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        textAlign: 'center',
+        backgroundColor: "#fff",
+        alignItems: "center",
+        textAlign: "center",
         // justifyContent: "center",
     },
     buttonView: {
-        marginTop: 20,
+        marginTop: 0,
         width: 150,
         borderRadius: 5,
-        textAlign: 'center',
+        textAlign: "center",
         height: 50,
     },
     anchoredButton: {
-        width: '100%',
+        width: "100%",
         flex: 1,
     },
-    nextCaseButton: {
-        backgroundColor: 'chartreuse',
-        color: 'black',
+    startButton: {
+        backgroundColor: "#26C281",
+        color: "black",
     },
-    noButton: {
-        backgroundColor: 'orangered',
-    },
-    okButton: {
-        backgroundColor: 'dodgerblue',
+    showSolutionButton: {
+        backgroundColor: "grey",
     },
     buttonText: {
-        height: '100%',
-        textAlignVertical: 'center',
-        textAlign: 'center',
+        height: "100%",
+        textAlignVertical: "center",
+        textAlign: "center",
+        color: "white",
+    },
+    bigButtonText: {
+        fontSize: 55,
     },
     descriptionText: {
         marginTop: 30,
         fontSize: 30,
     },
     header: {
-        fontSize: 60,
+        fontSize: 40,
         marginBottom: 20,
-        textAlign: 'center',
+        textAlign: "center",
     },
     algorithmText: {
         fontSize: 30,
         height: 150,
-        width: '90%',
-        textAlign: 'center',
+        width: "90%",
+        textAlign: "center",
+    },
+    scrambleText: {
+        marginTop: 50,
     },
     solutionView: {
         height: 200,
-        width: '100%',
-        alignItems: 'center',
+        width: "100%",
+        alignItems: "center",
     },
     timerText: {
-        fontSize: 20,
+        marginTop: 40,
+        fontSize: 50,
+    },
+    stopButton: {
+        backgroundColor: "#22A7F0",
     },
 });
 
@@ -76,11 +82,11 @@ enum TimeAttackButtonOption {
     NEXT_CASE,
 }
 
-function BottomScreenButton(props: {style: Object; onPress: () => void; text: string}) {
+function BottomScreenButton(props: {style: Object, onPress: () => void, text: string}) {
     return (
         <View style={[styles.anchoredButton, props.style]}>
             <TouchableNativeFeedback onPress={props.onPress} background={TOUCHABLE_BACKGROUND}>
-                <Text style={[props.style, styles.buttonText]}>{props.text}</Text>
+                <Text style={[props.style, styles.buttonText, styles.bigButtonText]}>{props.text}</Text>
             </TouchableNativeFeedback>
         </View>
     );
@@ -91,7 +97,7 @@ interface Props {
     navigation: any;
 }
 
-const TimeAttackPlayScreen: FC<Props> = (props) => {
+const TimeAttackPlayScreen: FC<Props> = props => {
     const caseStore = useContext(CaseStoreContext);
     const [currentCaseIndex, setCurrentCaseIndex] = useState(-1);
     const [scramble, setScramble] = useState(UNDEFINED_SCRAMBLE_TEXT);
@@ -103,12 +109,12 @@ const TimeAttackPlayScreen: FC<Props> = (props) => {
     const [solveCount, setSolveCount] = useState(0);
 
     function goToEndScreen() {
-        props.navigation.replace('TimeAttackEnd', {
+        props.navigation.replace("TimeAttackEnd", {
             totalTime: totalTime,
             cases: cases,
             solveCount: solveCount,
         });
-    };
+    }
 
     function stopTimer() {
         const timerStopTimestamp = new Date();
@@ -118,84 +124,88 @@ const TimeAttackPlayScreen: FC<Props> = (props) => {
         let caseTime = timerStopTimestamp.getTime() - timerStartTimestamp.getTime();
 
         setScramble(UNDEFINED_SCRAMBLE_TEXT);
-        setTotalTime(prevTotalTime => prevTotalTime + caseTime)
+        setTotalTime(prevTotalTime => prevTotalTime + caseTime);
         setTimerStartTimestamp(null);
         setSolveCount(prevSolveCount => prevSolveCount + 1);
         setCurrentCaseIndex(x => x + 1);
-    };
+    }
 
     function skipCase() {
         setTimerStartTimestamp(null);
         setCurrentCaseIndex(x => x + 1);
-    };
+    }
 
-    useEffect(function updateDisplayedCase() {
-        if (currentCaseIndex >= cases.length) {
-            goToEndScreen();
-            return;
-        }
+    useEffect(
+        function updateDisplayedCase() {
+            if (currentCaseIndex >= cases.length) {
+                goToEndScreen();
+                return;
+            }
 
-        const newCase = cases[currentCaseIndex];
+            const newCase = cases[currentCaseIndex];
 
-        setShouldDisplaySolution(false);
-        setButtonToDisplay(TimeAttackButtonOption.START_TIMER);
+            setShouldDisplaySolution(false);
+            setButtonToDisplay(TimeAttackButtonOption.START_TIMER);
 
-        if (newCase) {
-            GenerateScramble(newCase.algorithm, (success, scramble) => {
-                setScramble(success ? scramble : 'Error');
-            });
-        }
-    }, [currentCaseIndex, cases]);
+            if (newCase) {
+                GenerateScramble(newCase.algorithm, (success, scramble) => {
+                    setScramble(success ? scramble : "Error");
+                });
+            }
+        },
+        [currentCaseIndex, cases],
+    );
 
-    useEffect(function initializeCases() {
-        const {categories, shouldRandomlyAUF, shouldRandomlyMirror} = props.route.params;
+    useEffect(
+        function initializeCases() {
+            const {categories, shouldRandomlyAUF, shouldRandomlyMirror} = props.route.params;
+            // Detach the cases from mobx, so that when we randomly mirror and AUF, the actual cases don't get edited
+            let testedCases = ShuffleArray(caseStore.cases.map(x => Object.assign({}, x)));
 
-        // Detach the cases from mobx, so that when we randomly mirror and AUF, the actual cases don't get edited
-        let testedCases = ShuffleArray(caseStore.cases.map(x => Object.assign({}, x)));
+            if (categories.length > 0) {
+                testedCases = testedCases.filter(c => c.category && categories.includes(c.category));
+            }
 
-        if (categories.length > 0) {
-            testedCases = testedCases.filter(c => c.category && categories.includes(c.category));
-        }
-
-        if (shouldRandomlyMirror) {
-            for (const testedCase of testedCases) {
-                if (RandomChoice([true, false])) {
-                    testedCase.algorithm = MirrorAlgorithm(testedCase.algorithm);
+            if (shouldRandomlyMirror) {
+                for (const testedCase of testedCases) {
+                    if (RandomChoice([true, false])) {
+                        testedCase.algorithm = MirrorAlgorithm(testedCase.algorithm);
+                    }
                 }
             }
-        }
 
-        if (shouldRandomlyAUF) {
-            for (const testedCase of testedCases) {
-                const randomAUF = RandomChoice(["", "(U) ", "(U') ", "(U2) "]);
-                testedCase.algorithm = randomAUF + testedCase.algorithm;
+            if (shouldRandomlyAUF) {
+                for (const testedCase of testedCases) {
+                    const randomAUF = RandomChoice(["", "(U) ", "(U') ", "(U2) "]);
+                    testedCase.algorithm = randomAUF + testedCase.algorithm;
+                }
             }
-        }
 
-        setCases(testedCases);
-        setCurrentCaseIndex(0);
-    }, [caseStore.cases]);
+            setCases(testedCases);
+            setCurrentCaseIndex(0);
+        },
+        [caseStore.cases],
+    );
 
     function startTimer() {
         setButtonToDisplay(TimeAttackButtonOption.STOP_TIMER);
         setTimerStartTimestamp(new Date());
-    };
+    }
 
     function BottomButton() {
         switch (buttonToDisplay) {
             case TimeAttackButtonOption.STOP_TIMER:
-                return <BottomScreenButton text="Stop" onPress={stopTimer} style={styles.nextCaseButton} />;
+                return <BottomScreenButton text="Stop" onPress={stopTimer} style={styles.stopButton} />;
             case TimeAttackButtonOption.START_TIMER:
-                return <BottomScreenButton text="Start" onPress={startTimer} style={styles.nextCaseButton} />;
+                return <BottomScreenButton text="Start" onPress={startTimer} style={styles.startButton} />;
             case TimeAttackButtonOption.NEXT_CASE:
-                return <BottomScreenButton text="Next Case" onPress={skipCase} style={styles.nextCaseButton} />;
+                return <BottomScreenButton text="Next Case" onPress={skipCase} style={styles.stopButton} />;
         }
-    };
+    }
 
     return (
         <View style={styles.container}>
-            <Text style={styles.header}>Scramble:</Text>
-            <Text style={styles.algorithmText}>{scramble}</Text>
+            <Text style={[styles.scrambleText, styles.algorithmText]}>{scramble}</Text>
             <View style={styles.solutionView}>
                 {shouldDisplaySolution && cases[currentCaseIndex] ? (
                     [
@@ -208,26 +218,21 @@ const TimeAttackPlayScreen: FC<Props> = (props) => {
                     ]
                 ) : (
                     <>
-                        <View style={{height: 20}}>
-                            {timerStartTimestamp ? (
-                                <Timer
-                                    style={styles.timerText}
-                                    startTimestamp={timerStartTimestamp.getTime()}
-                                    extraTime={totalTime}
-                                />
-                            ) : (
-                                <Text style={styles.timerText}>{GetTimeText(totalTime)}</Text>
-                            )}
-                        </View>
-                        <View style={[styles.buttonView, styles.noButton]}>
-                            <TouchableNativeFeedback
+                        <View style={[styles.buttonView, styles.showSolutionButton]}>
+                            <TouchableWithoutFeedback
                                 onPress={() => {
                                     setShouldDisplaySolution(true);
                                     setButtonToDisplay(TimeAttackButtonOption.NEXT_CASE);
-                                }}
-                                background={TOUCHABLE_BACKGROUND}>
-                                <Text style={[styles.buttonText, {color: 'white'}]}>I forgot</Text>
-                            </TouchableNativeFeedback>
+                                }}>
+                                <Text style={styles.buttonText}>Show solution</Text>
+                            </TouchableWithoutFeedback>
+                        </View>
+                        <View style={{height: 20}}>
+                            {timerStartTimestamp ? (
+                                <Timer style={styles.timerText} startTimestamp={timerStartTimestamp.getTime()} extraTime={totalTime} />
+                            ) : (
+                                <Text style={styles.timerText}>{GetTimeText(totalTime)}</Text>
+                            )}
                         </View>
                     </>
                 )}
@@ -235,6 +240,6 @@ const TimeAttackPlayScreen: FC<Props> = (props) => {
             <BottomButton />
         </View>
     );
-}
+};
 
 export default observer(TimeAttackPlayScreen);
