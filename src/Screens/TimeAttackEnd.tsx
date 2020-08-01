e3677ac9240cfd216a877ad6381e3926bf86c675
Nitzan Bueno <nitzan.bueno@gmail.com>
2020-08-01 20:15:39 +0300

    Enlarge time attack end text

--- src/Screens/TimeAttackEnd.tsx
+++ src/Screens/TimeAttackEnd.tsx
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
         fontSize: 40,
     },
     descriptionText: {
-        fontSize: 25,
+        fontSize: 30,
         textAlign: "center",
     },
     highScoreView: {
