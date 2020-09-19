70fd554f1097281a0c92e2897b5ffbb52a3c9b9d
Nitzan Bueno <nitzan.bueno@gmail.com>
2020-09-19 13:49:27 +0300

    Auto-focus text prompt

--- src/CommonComponents/TextPrompt.tsx
+++ src/CommonComponents/TextPrompt.tsx
@@ -18,7 +18,7 @@ const TextPrompt: FC<Props> = props => {
             <Dialog visible={props.visible} onDismiss={props.onCancel}>
                 <Dialog.Title>{props.prompt}</Dialog.Title>
                 <Dialog.Content>
-                    <TextInput placeholder="e.g. OLL" mode="outlined" value={input} onChangeText={(value: string) => setInput(value)} />
+                    <TextInput autoFocus={true} placeholder="e.g. OLL" mode="outlined" value={input} onChangeText={(value: string) => setInput(value)} />
                 </Dialog.Content>
                 <Dialog.Actions>
                     <Button onPress={() => props.onSubmit(input)} children="OK" />
